c2051e0aff018adb31d4e4619930af21cb39b2e9
limemedesign <limemedesign@gmail.com>
2024-11-12 09:38:58 -0500

    final project - final working 100%

--- server.py
+++ server.py
@@ -1,36 +1,30 @@
+"""Module providing a function printing python version."""
+# Import Flask, render_template, request from the flask pramework package
+# Import the emotion_detection function from the package created
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
-#Initiate the flask app
 app = Flask("Emotion_Detector")
 
-
 @app.route("/emotionDetector")
-def emotionDetector():
-    # Retrieve the text to analyze 
+def emotional_detector():
+    """Function to analyze emotions from a given input."""
     text_to_analyze = request.args.get('textToAnalyze')
-
-    # Pass the text to the function and store the response
     response = emotion_detector(text_to_analyze)
-
     dominant_emotion = response['dominant_emotion']
-
     if dominant_emotion is None:
         return "Invalid text! Please try again!."
-    else:
-        # Generate the formatted string
-        emotion_str = ', '.join([f"'{emotion}': {score}" for emotion, score in response.items()]) 
-        
-        response_message = (
-            f"For the given statement, the system response is {emotion_str}. "
-            f"The dominant emotion is {dominant_emotion}."
-        )
 
-        return response_message
-
+    emotion_str = ', '.join([f"'{emotion}': {score}" for emotion, score in response.items()])
+    response_message = (
+        f"For the given statement, the system response is {emotion_str}. "
+        f"The dominant emotion is {dominant_emotion}."
+    )
+    return response_message
 
 @app.route("/")
 def render_index_page():
+    """Function rendering the index page."""
     return render_template('index.html')
 
 if __name__ == "__main__":
